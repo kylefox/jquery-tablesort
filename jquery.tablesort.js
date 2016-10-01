9a2fe320617f42d9bb6b688aa385019ccfeda2c1
Kyle Fox <kylefox@gmail.com>
2016-10-01 13:43:12 -0600

    remove commented line

--- jquery.tablesort.js
+++ jquery.tablesort.js
@@ -26,7 +26,6 @@
 				self = this,
 				table = this.$table,
 				rowsContainer = table.find('tbody').length > 0 ? table.find('tbody') : table,
-				//rows = this.$thead.length > 0 ? table.find('tbody tr') : table.find('tr').has('td'),
 				rows = rowsContainer.find('tr').has('td, th'),
 				cells = rows.find(':nth-child(' + (th.index() + 1) + ')').filter('td, th'),
 				sortBy = th.data().sortBy,
